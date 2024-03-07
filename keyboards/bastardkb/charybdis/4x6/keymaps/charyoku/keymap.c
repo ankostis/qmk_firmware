6dd501a8495600731604151fa2dcba33009938dc
Kostis Anagnostopoulos <ankostis@gmail.com>
2024-03-07 18:24:32 +0200

    feat(charyoku) restore Undo to previous place, Bckspc to its R

--- keyboards/bastardkb/charybdis/4x6/keymaps/charyoku/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/charyoku/keymap.c
@@ -132,7 +132,7 @@ enum my_keycodes {
  */
 #define LAYOUT_LAYER_FUNCTION                                                                          \
     ________________KEYB_CTRL_ROW_L________________,  ________________KEYB_CTRL_ROW_R________________, \
-    _______, KC_F12,  KC_F7,  KC_F8,  KC_F9,KC_PSCR,  KC_UNDO,KC_BSPC, KC_SPC,KC_SLSH,KC_QUOT,_______, \
+    _______, KC_F12,  KC_F7,  KC_F8,  KC_F9,KC_PSCR,  KC_BSPC,KC_UNDO, KC_SPC,KC_SLSH,KC_QUOT,_______, \
     _______, KC_F11,  KC_F4,  KC_F5,  KC_F6,KC_SCRL,  KC_PSTE,KC_LSFT,KC_LCTL,KC_LALT,KC_LGUI,_______, \
     _______, KC_F10,  KC_F1,  KC_F2,  KC_F3,KC_PAUS,   KC_CUT,KC_COPY,KC_MINS,KC_COMM, KC_DOT,_______, \
                              KC_APP,_______, KC_TAB,                                   KC_ENT,KC_BSPC, \
@@ -174,7 +174,7 @@ enum my_keycodes {
  */
 #define LAYOUT_LAYER_POINTER                                                                           \
     ________________KEYB_CTRL_ROW_L________________,  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,MA_TKFF, \
-    _________________DEAD_HALF_ROW_________________,  KC_UNDO,KC_BSPC,KC_BTN3,KC_BTN5,DPI_MOD,MA_GRWT, \
+    _________________DEAD_HALF_ROW_________________,  KC_BSPC,KC_UNDO,KC_BTN3,KC_BTN5,DPI_MOD,MA_GRWT, \
     ________________HOME_ROW_GACS_L________________,  KC_PSTE,KC_BTN2,KC_BTN1,KC_BTN4,MA_SCAL,MA_OFST, \
     _______,XXXXXXX,XXXXXXX,XXXXXXX,DRG_TOG,XXXXXXX,   KC_CUT,KC_COPY,DRGSCRL,_______,LA2_NAV,MA_LIMT, \
                              KC_ESC,_______,TAB_PTR,                                   KC_ENT,KC_BSPC, \
@@ -192,7 +192,7 @@ enum my_keycodes {
  */
 #define LAYOUT_LAYER_NAVIGATION                                                                        \
     ________________KEYB_CTRL_ROW_L________________,  ________________KEYB_CTRL_ROW_R________________, \
-    _________________DEAD_HALF_ROW_________________,  KC_UNDO,KC_BSPC,  KC_UP,KC_HOME,KC_PGUP, KC_TAB, \
+    _________________DEAD_HALF_ROW_________________,  KC_BSPC,KC_UNDO,  KC_UP,KC_HOME,KC_PGUP, KC_TAB, \
     ________________HOME_ROW_GACS_L________________,  KC_PSTE,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN, KC_SPC, \
     _______,KC_UNDO, KC_CUT,KC_COPY,XXXXXXX,KC_PSTE,   KC_CUT,KC_COPY, KC_INS,LA3_PTR,_______,_______, \
                              KC_ESC,_______,_______,                                   KC_ENT,KC_BSPC, \
@@ -210,7 +210,7 @@ enum my_keycodes {
  */
 #define LAYOUT_LAYER_NUMERAL                                                                           \
     ________________KEYB_CTRL_ROW_L________________,  ________________KEYB_CTRL_ROW_R________________, \
-    _______,KC_LBRC,   KC_7,   KC_8,   KC_9,KC_RBRC,  KC_UNDO,KC_BSPC, KC_SPC,KC_SLSH,KC_QUOT,_______, \
+    _______,KC_LBRC,   KC_7,   KC_8,   KC_9,KC_RBRC,  KC_BSPC,KC_UNDO, KC_SPC,KC_SLSH,KC_QUOT,_______, \
     _______,KC_SCLN,   KC_4,   KC_5,   KC_6, KC_EQL,  KC_PSTE,KC_LSFT,KC_LCTL,KC_LALT,KC_LGUI,_______, \
     _______, KC_GRV,   KC_1,   KC_2,   KC_3,KC_BSLS,   KC_CUT,KC_COPY,KC_MINS,KC_COMM, KC_DOT,_______, \
                              KC_DOT,   KC_0,KC_SLSH,                                   KC_ENT,_______, \
@@ -229,7 +229,7 @@ enum my_keycodes {
 */
 #define LAYOUT_LAYER_SYMBOLS                                                                           \
     ________________KEYB_CTRL_ROW_L________________,  ________________KEYB_CTRL_ROW_R________________, \
-    _______,KC_LCBR,KC_AMPR,KC_ASTR,KC_LPRN,KC_RCBR,  KC_UNDO,KC_BSPC, KC_SPC,KC_SLSH,KC_QUOT,_______, \
+    _______,KC_LCBR,KC_AMPR,KC_ASTR,KC_LPRN,KC_RCBR,  KC_BSPC,KC_UNDO, KC_SPC,KC_SLSH,KC_QUOT,_______, \
     _______,KC_COLN, KC_DLR,KC_PERC,KC_CIRC,KC_PLUS,  KC_PSTE,KC_LSFT,KC_LCTL,KC_LALT,KC_LGUI,_______, \
     _______,KC_TILD,KC_EXLM,  KC_AT,KC_HASH,KC_PIPE,   KC_CUT,KC_COPY,KC_MINS,KC_COMM, KC_DOT,_______, \
                             KC_LPRN,KC_RPRN,KC_QUES,                                  _______,KC_BSPC, \
